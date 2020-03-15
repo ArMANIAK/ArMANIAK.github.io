03a13b32c3397b20efa6dd7d0fedecd89f6a3a42
ArMANIAK <reklama.alatyr@gmail.com>
2020-03-15 17:06:50 +0200

    Few changes

--- Yestersand/script.js
+++ Yestersand/script.js
@@ -1,31 +1,10 @@
-//
-//                          Prototyping process
-//
-//          To-Do
-//    1. Rendering map
-//       1.1. Building an array of tiles
-//       1.2. Checking whether within borders
-//       1.3. Calculating and rendering from the center of the map
-//    2. Placement and movement of the character
-//    3. Interaction with objects and characters
-//    4. Skills
-//    5. Stats and Level-upping
-//    6. Generating objects
-//    7. Generating characters
-//    8. Day-night cycle
-//    9. Global and local Maps
-//   10. Inventory
-//   11. Using items
-//
-//
-
 'use strict'
 
 const TILE_SIZE = 75;
 
 const INFECTING_CHANCE = 0.5;
 
-const INFECTION_DEADLINESS = 0.3;
+const INFECTION_DEADLINESS = 0.02;
 
 const MEDICINE_EFFECTIVNESS = 0.7;
 
@@ -33,35 +12,31 @@ const LANDSCAPE_TYPES = [
     'forest',
     'sand',
     'rock',
+    'field',
     'mountain',
     'water',
-    'field',
     'deep_water',
 ];
-const MAP = [
-    ['deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water'],
-    ['deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water'],
-    ['deep_water', 'deep_water', 'sand', 'sand', 'sand', 'rock', 'rock', 'rock', 'rock', 'forest', 'forest', 'forest', 'sand', 'sand', 'forest', 'forest', 'rock', 'deep_water', 'deep_water'],
-    ['deep_water', 'deep_water', 'sand', 'sand', 'sand', 'sand', 'rock', 'rock', 'forest', 'forest', 'forest', 'forest', 'forest', 'sand', 'forest', 'forest', 'forest', 'deep_water', 'deep_water'],
-    ['deep_water', 'deep_water', 'sand', 'sand', 'rock', 'rock', 'rock', 'rock', 'rock', 'forest', 'forest', 'forest', 'sand', 'sand', 'forest', 'forest', 'rock', 'deep_water', 'deep_water'],
-    ['deep_water', 'deep_water', 'sand', 'sand', 'sand', 'rock', 'rock', 'rock', 'rock', 'forest', 'forest', 'forest', 'sand', 'sand', 'forest', 'forest', 'rock', 'deep_water', 'deep_water'],
-    ['deep_water', 'deep_water', 'sand', 'sand', 'sand', 'sand', 'sand', 'rock', 'rock', 'forest', 'forest', 'forest', 'sand', 'sand', 'forest', 'forest', 'rock', 'deep_water', 'deep_water'],
-    ['deep_water', 'deep_water', 'sand', 'sand', 'sand', 'rock', 'sand', 'rock', 'rock', 'forest', 'forest', 'forest', 'sand', 'sand', 'forest', 'forest', 'rock', 'deep_water', 'deep_water'],
-    ['deep_water', 'deep_water', 'sand', 'sand', 'sand', 'sand', 'rock', 'rock', 'rock', 'forest', 'forest', 'forest', 'sand', 'sand', 'forest', 'forest', 'rock', 'deep_water', 'deep_water'],
-    ['deep_water', 'deep_water', 'sand', 'sand', 'sand', 'sand', 'sand', 'rock', 'rock', 'forest', 'forest', 'forest', 'sand', 'sand', 'forest', 'forest', 'rock', 'deep_water', 'deep_water'],
-    ['deep_water', 'deep_water', 'sand', 'sand', 'sand', 'rock', 'sand', 'rock', 'sand', 'forest', 'forest', 'forest', 'sand', 'forest', 'forest', 'forest', 'rock', 'deep_water', 'deep_water'],
-    ['deep_water', 'deep_water', 'sand', 'sand', 'sand', 'rock', 'sand', 'sand', 'sand', 'forest', 'forest', 'forest', 'forest', 'forest', 'forest', 'forest', 'rock', 'deep_water', 'deep_water'],
-    ['deep_water', 'deep_water', 'sand', 'sand', 'sand', 'rock', 'rock', 'sand', 'rock', 'forest', 'forest', 'forest', 'sand', 'sand', 'forest', 'forest', 'rock', 'deep_water', 'deep_water'],
-    ['deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water'],
-    ['deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water', 'deep_water'],
-];
+const MAP_SIZE = 30;
 
 const DIRECTIONS = [
     'left',
     'right',
     'up',
     'down',
-]
+];
+
+let gameStarted = true;
+
+const mainScreen = document.querySelector('main > div.field');
+const aside = document.querySelector('.aside');
+const moveControl = document.querySelector('.controls');
+const debugChat = document.querySelector('content');
+var height = mainScreen.scrollHeight;
+var width = mainScreen.scrollWidth;
+var x_tiles = Math.floor(width / TILE_SIZE);
+var y_tiles = Math.floor(height / TILE_SIZE);  //  defining the quantity of tiles on each axis
+// debugChat.innerHTML += ('<p>' + x_tiles + 'x' + y_tiles + '</p>');
 
 class Tile {
     constructor(type = 'deep_water') { 
@@ -74,11 +49,23 @@ class Tile {
 
 const buildMap = () => {
     let worldMap = new Array;
-    for (let y in MAP) {
+    for (let y = 0; y < MAP_SIZE; y++) {
         let row = new Array;
-        for (let x in MAP[y]) {
-            let tile = new Tile(MAP[y][x]);
-            row.push(tile);
+        if (y < 2 || y > MAP_SIZE - 3) {
+            row = new Array(MAP_SIZE).fill(new Tile);
+            worldMap.push(row);
+            continue;
+        }
+        else {
+            for (let x = 0; x < MAP_SIZE; x++) {
+                let tile;
+                if (x < 2 || x > MAP_SIZE - 3) {
+                    row.push(new Tile);
+                }
+                else {
+                    row.push(new Tile(LANDSCAPE_TYPES[Math.floor(Math.random() * 4)]));
+                }
+            }
         }
         worldMap.push(row);
     }
@@ -117,85 +104,162 @@ class Character {
     }
 }
 
+const printStatistics = () => {
+    aside.innerHTML = '';
+    for (let prop in STATISTICS) {
+        if (typeof STATISTICS[prop] != 'function') {
+            let element = document.createElement('p');
+            element.innerText = prop + ': ' + STATISTICS[prop];
+            aside.appendChild(element);
+        }
+    }
+}
+
 const isSuccess = (chance) => {
     return Math.random() > chance ? true : false;
 }
 
 const gameOver = (reason) => {
-    aside = '';
+    gameStarted = false;
+    aside.innerText = '';
+    console.log(reason);
     let element = document.createElement('h2');
-    element.innerText = 'Игра окончена! Вы умерли из-за ' + reason + '<br/> Вы стали ' + STATISTICS.deaths + ' жертвой эпидемии';
+    if (reason == 1) {
+        console.log('reason - coronavirus');
+        element.innerText = 'Эпидемия побеждена ценой ' + STATISTICS.deaths + ' жизней';
+    }
+    else {
+        console.log('reason - panic');
+        element.innerText = 'Игра окончена! Вы умерли из-за ' + reason; 
+        //Вы стали ' + STATISTICS.deaths + ' жертвой эпидемии';
+    }
+    // printStatistics();
+    console.log(reason);
+    console.log(element);
     aside.appendChild(element);
-    renderScreen = null;
 }
 
 const checkNearby = (object, x, y) => {    
-    console.log('check nearby ' + x + ': ' + y);
     if (map[y][x].filled) {
         let neighbour = JSON.parse(map[y][x].filled);
+
+// ПАНИЧЕСКАЯ АТАКА
+
         if (object.view == 'panic' && neighbour.view == 'infectious') {
-            if (isSuccess(1 - object.immunity)) {
+            if (isSuccess(1 - (object.immunity - neighbour.immunity))) {
                 STATISTICS.total--;
                 STATISTICS.infectious--;
                 STATISTICS.deaths++;
                 map[y][x].filled = null;
+                debugChat.innerHTML += ('<p>Еще один человек пал жертвой паники!</p>');
                 if (x == hero.x_coord && y == hero.y_coord) gameOver('паники');
                 return;
             }
         }
-        if ((object.view == 'clean' || object.view == 'panic') && neighbour.view == 'infectious') {
-            if (isSuccess(INFECTING_CHANCE * (1 - object.immunity))) {
-                STATISTICS.clean--;
-                STATISTICS.infectious++;
-                object.view = 'infectious';
+        if (object.view == 'infectious' && neighbour.view == 'panic') {
+            if (isSuccess(1 - (neighbour.immunity - object.immunity))) {
+                STATISTICS.total--;
+                STATISTICS.infectious--;
+                STATISTICS.deaths++;
+                map[object.y_coord][object.x_coord].filled = null;
+                debugChat.innerHTML += ('<p>Еще один человек пал жертвой паники!</p>');
+                if (object.x_coord == hero.x_coord && object.y_coord == hero.y_coord) gameOver('паники');
+                return;
             }
         }
+
+// ЗАРАЖЕНИЕ
+
+        if ((object.view == 'clean' || object.view == 'panic') && neighbour.view == 'infectious') {
+            if (isSuccess(INFECTING_CHANCE * (1 - object.immunity))) {
+                if (object.view == 'panic') STATISTICS.clean--;
+                STATISTICS[object.view]--;
+                STATISTICS.infectious++;
+                object.view = 'infectious';
+                debugChat.innerHTML += ('<p>Зарегистрирован еще один случай заражения коронавирусом</p>');
+            }
+        }
+        if ((neighbour.view == 'clean' || neighbour.view == 'panic') && object.view == 'infectious') {
+            if (isSuccess(INFECTING_CHANCE * (1 - neighbour.immunity))) {
+                STATISTICS[neighbour.view]--;
+                if (neighbour.view == 'panic') STATISTICS.clean--;
+                STATISTICS.infectious++;
+                neighbour.view = 'infectious';
+                debugChat.innerHTML += ('<p>Зарегистрирован еще один случай заражения коронавирусом</p>');
+            }
+        }
+
+// ИЗЛЕЧЕНИЕ ЧЕРЕЗ ЦКЗ/ВОЗ
+
         if (object.view == 'cleaner' && neighbour.view == 'infectious') {
             if (isSuccess(1 - MEDICINE_EFFECTIVNESS)) {
                 STATISTICS.clean++;
                 STATISTICS.infectious--;
                 neighbour.view = 'infectious';
+                debugChat.innerHTML += ('<p>Излечение возможно. Новый случай излечения зарегистрирован</p>');
             }
         }
         map[y][x].filled = JSON.stringify(neighbour);
     }
 }
 
-const testPerson = (x, y) => {
-    console.log('test person ' + x + ': ' + y);
-    let object = JSON.parse(map[y][x].filled);
-    if (object.view == 'infectious' && isSuccess(1 - object.immunity)) {
+const testPerson = (person) => {
+
+// ИЗЛЕЧЕНИЕ
+
+    if (person.view == 'infectious' && isSuccess(1 - person.immunity)) {
         STATISTICS.clean++;
         STATISTICS.infectious--;
-        object.view = 'clean';
+        person.view = 'clean';
+        debugChat.innerHTML += ('<p>Излечение возможно. Новый случай излечения зарегистрирован</p>');
     }
-    if (object.view == 'infectious' && isSuccess(INFECTION_DEADLINESS)) {
+
+// СМЕРТЬ ОТ ВИРУСА
+
+    if (person.view == 'infectious' && isSuccess(1 - INFECTION_DEADLINESS)) {
         STATISTICS.deaths++;
         STATISTICS.total--;
         STATISTICS.infectious--;
-        map[y][x].filled = null;
-        if (x == hero.x_coord && y == hero.y_coord) gameOver('коронавируса');
+        map[person.y_coord][person.x_coord].filled = null;
+        debugChat.innerHTML += ('<p>Коронавирус продолжает кровавую жатву! Зарегистрирован случай смерти от болезни</p>');
+        if (person.x_coord == hero.x_coord && person.y_coord == hero.y_coord) gameOver('коронавируса');
         return;
     }
-    if (object.view == 'clean' && isSuccess((STATISTICS.infectious + STATISTICS.deaths) / STATISTICS.total)) {
+
+// ПАНИКА
+
+    if (person.view == 'clean' && isSuccess((STATISTICS.infectious + STATISTICS.deaths) / STATISTICS.total)) {
         STATISTICS.panic++;
-        object.view = 'panic';
+        person.view = 'panic';
     }
-    checkNearby(object, parseInt(x) + 1, parseInt(y));
-    checkNearby(object, parseInt(x), parseInt(y) + 1);
-    map[y][x].filled = JSON.stringify(object);
+
+// ПРОВЕРКА СОСЕДЕЙ
+
+    checkNearby(person, parseInt(person.x_coord) + 1, parseInt(person.y_coord));
+    checkNearby(person, parseInt(person.x_coord), parseInt(person.y_coord) + 1);
+    map[person.y_coord][person.x_coord].filled = JSON.stringify(person);
+    if (STATISTICS.infectious == 0) gameOver(1);
 }
 
+const changeImmunity = (person) => {
+    person.immunity = Math.random() > 0.5 ? person.immunity + 0.01 : person.immunity - 0.01;
+} 
+
 const moveOn = () => {
+    let alreadyMoved = [];
     for (let row in map) {
         for (let column in map[row]) {
             let tile = map[row][column];
-            if (tile.filled) testPerson(column, row);
             if (tile.filled) {
+                let person = JSON.parse(tile.filled);
+                changeImmunity(person);
+                testPerson(person);
                 if (hero.x_coord != column && hero.y_coord != row) {
-                    let person = JSON.parse(tile.filled);
-                    let direction = DIRECTIONS[Math.floor(Math.random() * 4)];
-                    person.move(direction);
+                    if (alreadyMoved.indexOf(person == -1)) {
+                        let direction = DIRECTIONS[Math.floor(Math.random() * 4)];
+                        person.move(direction);
+                        alreadyMoved.push(JSON.stringify(person));
+                    }
                 }
             }
         }
@@ -228,17 +292,7 @@ const checkIndex = (x, y) => {
     return map[y][x];
 }
 
-const mainScreen = document.querySelector('main > div.field');
-const aside = document.querySelector('.aside');
-const moveControl = document.querySelector('.controls');
-const debugChat = document.querySelector('content');
-var height = mainScreen.scrollHeight;
-var width = mainScreen.scrollWidth;
-var x_tiles = Math.floor(width / TILE_SIZE);
-var y_tiles = Math.floor(height / TILE_SIZE);  //  defining the quantity of tiles on each axis
-debugChat.innerHTML += ('<p>' + x_tiles + 'x' + y_tiles + '</p>');
-
-const renderScreen = (x, y) => {
+let renderScreen = (x, y) => {
     mainScreen.innerHTML = '';
     let x_start = x - Math.floor(x_tiles / 2); 
     let x_finish = x_start + x_tiles;
@@ -263,19 +317,13 @@ const renderScreen = (x, y) => {
             mainScreen.appendChild(element);
         }
     }
-    aside.innerHTML = '';
-    for (let prop in STATISTICS) {
-        if (typeof prop != 'function') {
-            let element = document.createElement('p');
-            element.innerText = prop + ': ' + STATISTICS[prop];
-            aside.appendChild(element);
-        }
-    }
+    printStatistics();
 }
 
 let viewport_x_center = 3;
 let viewport_y_center = 2;  // defining coordinates of the center of the screen
-let hero = new Character(3, 2, 'clean');
+let hero = {x_coord: 3, y_coord: 2};
+new Character(3, 2, 'clean');
 renderScreen(hero.x_coord, hero.y_coord);
 
 const isTileFilled = (x, y) => {
@@ -312,33 +360,43 @@ Object.prototype.move = function (direction) {
 }
 
 document.body.addEventListener('keypress', event => {
-    moveOn();
-    switch (event.keyCode) {
-        case 100:
-        case 1074:
-            hero.move('right');
-            break;
-        case 97:
-        case 1092:
-            hero.move('left');
-            break;
-        case 115:
-        case 1099:
-            hero.move('down');
-            break;
-        case 119:
-        case 1094:
-            hero.move('up');
-            break;
+    let player = JSON.parse(map[hero.y_coord][hero.x_coord].filled);
+    if (gameStarted && player) {
+        moveOn();
+        switch (event.keyCode) {
+            case 100:
+            case 1074:
+                player.move('right');
+                break;
+            case 97:
+            case 1092:
+                player.move('left');
+                break;
+            case 115:
+            case 1099:
+                player.move('down');
+                break;
+            case 119:
+            case 1094:
+                player.move('up');
+                break;
+        }
+        hero.x_coord = player.x_coord;
+        hero.y_coord = player.y_coord;
+        renderScreen(hero.x_coord, hero.y_coord);
     }
-    renderScreen(hero.x_coord, hero.y_coord);
 });
 
 moveControl.addEventListener('click', function (e) {
-    e.stopPropagation();
-    moveOn();
-    hero.move(e.target.getAttribute('data-direction'));
-    renderScreen(hero.x_coord, hero.y_coord);
+    if (gameStarted) {
+        e.stopPropagation();
+        moveOn();
+        let player = JSON.parse(map[hero.y_coord][hero.x_coord].filled);
+        player.move(e.target.getAttribute('data-direction'));
+        hero.x_coord = player.x_coord;
+        hero.y_coord = player.y_coord;
+        renderScreen(hero.x_coord, hero.y_coord);
+    }    
 });
 
 window.onresize = () => {
